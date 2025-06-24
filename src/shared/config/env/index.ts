9db917a7eff3e84e41029c26d5bcc144b1075f28
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-06-24 10:56:39 -0300

    feat: add prisma client instance

--- src/shared/config/env/index.ts
+++ src/shared/config/env/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import z from "zod"
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["dev", "test"]),
+	NODE_ENV: z.enum(["dev", "test", "production"]),
 	PORT: z.string().transform(Number).pipe(z.number().min(1).max(65535))
 })
 
