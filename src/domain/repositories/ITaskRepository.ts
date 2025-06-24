6d0a30ae3b791ea1ba4566998d6b2b76b33cc23b
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-06-24 10:37:52 -0300

    refactor: add option null in return of task repository

--- src/domain/repositories/ITaskRepository.ts
+++ src/domain/repositories/ITaskRepository.ts
@@ -5,6 +5,10 @@ import type { Task } from "../models/Task"
 export interface ITaskRepository {
 	create(task: CreateTaskDTO, userId: string): Promise<Task>
 	findByUserId(userId: string): Promise<Task[]>
-	update(taskId: string, userId: string, task: UpdateTaskDTO): Promise<Task>
-	delete(taskId: string, userId: string): Promise<Task>
+	update(
+		taskId: string,
+		userId: string,
+		task: UpdateTaskDTO
+	): Promise<Task | null>
+	delete(taskId: string, userId: string): Promise<Task | null>
 }
